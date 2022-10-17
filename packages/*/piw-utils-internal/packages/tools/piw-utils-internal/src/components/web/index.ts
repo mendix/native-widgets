5fd7604f1251a47bb24b484709e28f12ef1a469a
Diego Antonelli <diego.antonelli@mendix.com>
2022-10-17 16:51:05 +0200

    chore: move piw-tools

--- packages/*/piw-utils-internal/packages/tools/piw-utils-internal/src/components/web/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./components";
-export * from "./FilterSelector";
-export * from "./InfiniteBody";
-export * from "./Pagination";
-export * from "./usePositionObserver";
-export * from "./utils";
-export * from "./preview";
--- packages/*/piw-utils-internal/packages/tools/piw-utils-internal/src/components/web/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./dom";
-export * from "./FilterProvider";
-export * from "./SortProvider";
-export * from "./generateUUID";
