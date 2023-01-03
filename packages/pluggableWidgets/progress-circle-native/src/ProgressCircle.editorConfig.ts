4b1f990b829bfa52079b4f95ee7c1bbe3f86edae
Uraz Akgultan <uraz.akgultan@gmail.com>
2023-01-03 07:56:57 +0100

    feat(progress-circle-native): add accessibility

--- packages/pluggableWidgets/progress-circle-native/src/ProgressCircle.editorConfig.ts
+++ packages/pluggableWidgets/progress-circle-native/src/ProgressCircle.editorConfig.ts
@@ -1,4 +1,6 @@
 import { StructurePreviewProps } from "@mendix/piw-utils-internal";
+import { hidePropertyIn, Properties } from "@mendix/pluggable-widgets-tools";
+
 import StructurePreviewSvg from "./assets/structure-preview.svg";
 import StructurePreviewSvgDark from "./assets/structure-preview-dark.svg";
 import { ProgressCirclePreviewProps } from "../typings/ProgressCircleProps";
@@ -12,4 +14,13 @@ export function getPreview(_: ProgressCirclePreviewProps, isDarkMode: boolean): 
         height: 175,
         width: 175
     };
+}
+
+export function getProperties(values: ProgressCirclePreviewProps, defaultProperties: Properties): Properties {
+    if (values.accessible === "no") {
+        hidePropertyIn(defaultProperties, values, "screenReaderCaption");
+        hidePropertyIn(defaultProperties, values, "screenReaderHint");
+    }
+
+    return defaultProperties;
 }
