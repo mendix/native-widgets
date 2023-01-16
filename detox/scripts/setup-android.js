fca9da1d18bfdc292ab05cd5b1e28c2ff2c86877
Uraz Akgultan <uraz@akgultan.com>
2023-01-16 18:48:04 +0100

    fix: fix e2e scripts

--- detox/scripts/setup-android.js
+++ detox/scripts/setup-android.js
@@ -7,19 +7,37 @@ main().catch(e => {
 });
 
 async function main() {
+    let targetProcessor = "x86_64";
+
+    process.on("exit", exitCode => {
+        return exitCode > 0 && console.log(`exiting the code implicitly ${exitCode}`);
+    });
+
+    process.argv.forEach(val => {
+        if (val.startsWith("--target")) {
+            const targetSliced = val.slice(val.indexOf("=") + 1, val.length);
+            targetProcessor = targetSliced && targetSliced;
+        }
+    });
+
+    if (!targetProcessor) {
+        console.error(`Target processor (${targetProcessor}) is not valid!`);
+        process.exit(1);
+    }
+
     console.log("Downloading Android apps...");
     await Promise.all([
-        downloadFile("https://www.dropbox.com/s/4hel4yki6vl1et5/app-detox-debug-androidTest.apk?dl=1"),
-        downloadFile("https://www.dropbox.com/s/qu4z1kpy8abnyr3/app-detox-debug.apk?dl=1")
+        downloadFile("https://www.dropbox.com/s/18szmaf73l1docj/app-detox-debug-androidTest.apk?dl=1"),
+        downloadFile("https://www.dropbox.com/s/rya9h4fohvhl2fl/app-detox-debug.apk?dl=1")
     ]);
 
-    console.log(`Installing Android SDK version ${ANDROID_SDK_VERSION}...`);
-    execCommand(`sdkmanager 'system-images;android-${ANDROID_SDK_VERSION};google_apis;x86_64'`);
+    console.log(`Installing Android SDK version ${ANDROID_SDK_VERSION}, target ${targetProcessor}...`);
+    execCommand(`sdkmanager 'system-images;android-${ANDROID_SDK_VERSION};google_apis;${targetProcessor}'`);
     execCommand("sdkmanager --licenses");
 
     console.log("Creating Android emulator...");
     execCommand(
-        `avdmanager -s create avd -n NATIVE_${ANDROID_DEVICE_TYPE}_${ANDROID_SDK_VERSION} -k 'system-images;android-${ANDROID_SDK_VERSION};google_apis;x86_64' -f -d '${ANDROID_DEVICE_TYPE}' -c 1000M`
+        `avdmanager -s create avd -n NATIVE_${ANDROID_DEVICE_TYPE}_${ANDROID_SDK_VERSION} -k 'system-images;android-${ANDROID_SDK_VERSION};google_apis;${targetProcessor}' -f -d '${ANDROID_DEVICE_TYPE}' -c 1000M`
     );
 
     console.log("Done!");
