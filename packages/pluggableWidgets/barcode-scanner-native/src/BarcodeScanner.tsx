a5c3d1bbcc890c3cf71c7e5fd36da6dfd7bc4890
Daan Verweij <daan.verweij@emixa.com>
2024-04-25 13:30:06 +0200

    Inverted type check based on comment furkanarabaci

--- packages/pluggableWidgets/barcode-scanner-native/src/BarcodeScanner.tsx
+++ packages/pluggableWidgets/barcode-scanner-native/src/BarcodeScanner.tsx
@@ -21,7 +21,7 @@ export class BarcodeScanner extends Component<Props> {
                 <RNCamera
                     testID={this.props.name}
                     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-                    type={this.props.cameraType  == 'back' ? RNCamera.Constants.Type.back : RNCamera.Constants.Type.front}
+                    type={this.props.cameraType  === 'front' ? RNCamera.Constants.Type.front : RNCamera.Constants.Type.back}
                     captureAudio={false}
                     onBarCodeRead={this.onBarCodeReadHandler}
                 >
