2bd89babd52473b1b9c90d4130fc5c51ee087215
Nikola Simsic <nikola.simsic@mendix.com>
2024-12-19 16:04:42 +0100

    Debug maestro

--- maestro/run_maestro.sh
+++ maestro/run_maestro.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ "$1" == "android" ]; then
-  APP_ID="myapp.nativecomponentstestproject"
+  APP_ID="com.mendix.native.template"
 elif [ "$1" == "ios" ]; then
   APP_ID="com.mendix.native.template"
 else
   echo "Usage: $0 [android|ios]"
   exit 1
 fi
-maestro test --env APP_ID=$APP_ID ../packages/pluggableWidgets/app-events-native/e2e/specs/maestro/AppEvents.yaml
+
+$HOME/.local/bin/maestro/bin/maestro test --env APP_ID=$APP_ID packages/pluggableWidgets/app-events-native/e2e/specs/maestro/AppEvents.yaml
