67ec89de1b11a74ac92e1657a6774e3c0744456f
Nikola Simsic <nikola.simsic@mendix.com>
2025-02-20 15:14:23 +0100

    Restart android emulator when test fails and rety

--- maestro/run_maestro_tests.sh
+++ maestro/run_maestro_tests.sh
@@ -28,21 +28,44 @@ yaml_test_files=($(find $search_path -type f -path "*/maestro/*.yaml" ! -name "*
 total_tests=${#yaml_test_files[@]}
 completed_tests=0
 
+MAX_RETRIES=3
+RETRY_DELAY=10
+
+# Function to restart the emulator
+restart_emulator() {
+    echo "Restarting emulator..."
+    adb -s emulator-5554 emu kill
+    sleep 10
+    nohup emulator -avd test -no-window -gpu swiftshader_indirect -no-boot-anim -no-snapshot -memory 4096 -cores 4 &
+    sleep 60
+}
+
 # Execute each YAML test file
 for yaml_test_file in "${yaml_test_files[@]}"; do
   echo "🔄 Running test: $yaml_test_file"
-  completed_tests=$((completed_tests + 1))
-  remaining_tests=$((total_tests - completed_tests))
-  
-  if $HOME/.local/bin/maestro/bin/maestro test --env APP_ID=$APP_ID --env PLATFORM=$PLATFORM --env MAESTRO_DRIVER_STARTUP_TIMEOUT=300000 "$yaml_test_file"; then
-    echo "✅ Test passed: $yaml_test_file"
-    passed_tests+=("$yaml_test_file")
-  else
-    echo "❌ Test failed: $yaml_test_file"
-    failed_tests+=("$yaml_test_file")
-  fi
-  
-  echo "Progress: $completed_tests/$total_tests tests completed, $remaining_tests tests remaining. ✅ ${#passed_tests[@]} passed, ❌ ${#failed_tests[@]} failed."
+  RETRIES=0
+
+  while [ "$RETRIES" -lt "$MAX_RETRIES" ]; do
+    completed_tests=$((completed_tests + 1))
+    remaining_tests=$((total_tests - completed_tests))
+    
+    if $HOME/.local/bin/maestro/bin/maestro test --env APP_ID=$APP_ID --env PLATFORM=$PLATFORM --env MAESTRO_DRIVER_STARTUP_TIMEOUT=300000 "$yaml_test_file"; then
+      echo "✅ Test passed: $yaml_test_file"
+      passed_tests+=("$yaml_test_file")
+      break
+    else
+      echo "❌ Test failed: $yaml_test_file"
+      failed_tests+=("$yaml_test_file")
+      RETRIES=$((RETRIES + 1))
+      if [ "$RETRIES" -lt "$MAX_RETRIES" ]; then
+        echo "Retrying in $RETRY_DELAY seconds..."
+        sleep "$RETRY_DELAY"
+        restart_emulator
+      fi
+    fi
+    
+    echo "Progress: $completed_tests/$total_tests tests completed, $remaining_tests tests remaining. ✅ ${#passed_tests[@]} passed, ❌ ${#failed_tests[@]} failed."
+  done
 done
 
 echo
