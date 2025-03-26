a49346d3f18b17e8cacacc2de4cb4ec9e0a44f2e
Harun.Karahan <harun.karahan@mendix.com>
2025-03-26 17:53:50 +0100

    update url adress

--- packages/jsActions/mobile-resources-native/src/camera/SaveToPictureLibrary.ts
+++ packages/jsActions/mobile-resources-native/src/camera/SaveToPictureLibrary.ts
@@ -34,7 +34,7 @@ export async function SaveToPictureLibrary(picture?: mendix.lib.MxObject): Promi
     // Save the file as a photo to the camera roll.
     try {
         const savedUri = await CameraRoll.saveToCameraRoll(url, "auto");
-        return Promise.resolve(savedUri as any as string);
+        return Promise.resolve(savedUri.node.image.uri);
     } catch (error) {
         return Promise.reject(error);
     }
