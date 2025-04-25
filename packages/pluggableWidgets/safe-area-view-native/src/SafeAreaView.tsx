33008b5dc2ad2d65f3e14b800933abb81557699e
Harun.Karahan <harun.karahan@mendix.com>
2025-04-25 14:30:45 +0200

    migrated safeAreaView to react-native-safe-area-context

--- packages/pluggableWidgets/safe-area-view-native/src/SafeAreaView.tsx
+++ packages/pluggableWidgets/safe-area-view-native/src/SafeAreaView.tsx
@@ -1,22 +1,33 @@
 import { createElement } from "react";
-import { SafeAreaView as ReactSaveAreaView, View } from "react-native";
+import { View } from "react-native";
 import { flattenStyles } from "@mendix/piw-native-utils-internal";
 
 import { SafeAreaViewStyle, defaultSafeAreaViewStyle } from "./ui/Styles";
 import { SafeAreaViewProps } from "../typings/SafeAreaViewProps";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export const SafeAreaView = (props: SafeAreaViewProps<SafeAreaViewStyle>): JSX.Element => {
     const styles = flattenStyles(defaultSafeAreaViewStyle, props.style);
+    const insets = useSafeAreaInsets();
 
     return (
-        <ReactSaveAreaView
-            style={{ flex: 1, ...{ backgroundColor: styles.container.backgroundColor } }}
+        <View
+            style={{
+                flex: 1,
+                ...{
+                    backgroundColor: styles.container.backgroundColor,
+                    marginTop: insets.top,
+                    marginBottom: insets.bottom,
+                    marginLeft: insets.left,
+                    marginRight: insets.right
+                }
+            }}
             pointerEvents={"box-none"}
             testID={props.name}
         >
             <View style={styles.container} pointerEvents={"box-none"}>
                 {props.content}
             </View>
-        </ReactSaveAreaView>
+        </View>
     );
 };
