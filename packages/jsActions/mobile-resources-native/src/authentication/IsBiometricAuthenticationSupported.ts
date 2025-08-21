be11ad1a94ee489608d95cbbc40e544d54902c20
Yogendra Shelke <yogendrashelke1992@gmail.com>
2025-08-21 11:09:21 +0530

    chore: upgrade react-native-biometrics dependancy
    upgrade react-native-biometrics dependancy

--- packages/jsActions/mobile-resources-native/src/authentication/IsBiometricAuthenticationSupported.ts
+++ packages/jsActions/mobile-resources-native/src/authentication/IsBiometricAuthenticationSupported.ts
@@ -5,7 +5,7 @@
 // - the code between BEGIN USER CODE and END USER CODE
 // - the code between BEGIN EXTRA CODE and END EXTRA CODE
 // Other code you write will be lost the next time you deploy the project.
-import ReactNativeBiometrics from "react-native-biometrics";
+import { isSensorAvailable } from "@sbaiahmed1/react-native-biometrics";
 
 // BEGIN EXTRA CODE
 // END EXTRA CODE
@@ -15,12 +15,9 @@ import ReactNativeBiometrics from "react-native-biometrics";
  */
 export async function IsBiometricAuthenticationSupported(): Promise<boolean> {
     // BEGIN USER CODE
-    // Documentation https://github.com/smallcase/react-native-simple-biometrics
+    // Documentation https://github.com/sbaiahmed1/react-native-biometrics
 
-    const rnBiometrics = new ReactNativeBiometrics();
-
-    return rnBiometrics
-        .isSensorAvailable()
+    return isSensorAvailable()
         .then(result => result.available)
         .catch(() => false);
 
