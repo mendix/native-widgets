ff7acf7f8f34855ca2cc5237a28cecd5553f0e8a
MOO <moo@mendix.com>
2026-05-06 13:32:49 +0000

    chore(nanoflow-actions-native): update changelogs

--- packages/pluggableWidgets/feedback-native/typings/FeedbackProps.d.ts
+++ packages/pluggableWidgets/feedback-native/typings/FeedbackProps.d.ts
@@ -37,7 +37,7 @@ export interface FeedbackPreviewProps {
     renderMode?: "design" | "xray" | "structure";
     sprintrapp: string;
     allowScreenshot: boolean;
-    logo: { type: "static"; imageUrl: string } | { type: "dynamic"; entity: string } | null;
+    logo: { type: "static"; imageUrl: string; } | { type: "dynamic"; entity: string; } | null;
     titleSendFeedback: string;
     titleSending: string;
     titleResult: string;
