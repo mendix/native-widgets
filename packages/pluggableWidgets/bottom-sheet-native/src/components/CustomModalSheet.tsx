78e66c2965452eb2a53d036eb73f7ac93c8f5eb9
MxKevinBeqo <kevin.beqo@mendix.com>
2026-06-23 13:52:41 +0200

    fix(bottom-sheet-native): re-introduce Pressable as 'close when click outside' mechanism

--- packages/pluggableWidgets/bottom-sheet-native/src/components/CustomModalSheet.tsx
+++ packages/pluggableWidgets/bottom-sheet-native/src/components/CustomModalSheet.tsx
@@ -1,6 +1,5 @@
 import { ReactElement, ReactNode, useCallback, useRef, useState } from "react";
-import { Modal, useWindowDimensions } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Modal, Pressable, useWindowDimensions } from "react-native";
 import BottomSheet, {
     BottomSheetBackdrop,
     BottomSheetBackdropProps,
@@ -57,45 +56,45 @@ export const CustomModalSheet = (props: CustomModalSheetProps): ReactElement => 
 
     const renderBackdrop = useCallback(
         (backdropProps: BottomSheetBackdropProps) => (
-            <BottomSheetBackdrop
-                {...backdropProps}
-                pressBehavior="close"
-                opacity={0.3}
-                appearsOnIndex={0}
-                disappearsOnIndex={-1}
-            />
+            <Pressable style={{ flex: 1 }} onPress={close}>
+                <BottomSheetBackdrop
+                    {...backdropProps}
+                    pressBehavior="close"
+                    opacity={0.3}
+                    appearsOnIndex={0}
+                    disappearsOnIndex={-1}
+                />
+            </Pressable>
         ),
-        []
+        [close]
     );
 
     const maxHeight = windowHeight * 0.9;
 
     return (
         <Modal transparent animationType="none" visible={mounted} onRequestClose={close} onShow={handleModalShow}>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-                {ready && (
-                    <BottomSheet
-                        ref={bottomSheetRef}
-                        index={0}
-                        animateOnMount
-                        enableDynamicSizing
-                        maxDynamicContentSize={maxHeight}
-                        containerHeight={windowHeight}
-                        enablePanDownToClose
-                        onChange={handleChange}
-                        onClose={() => handleChange(-1)}
-                        backdropComponent={renderBackdrop}
-                        style={[props.styles.modal]}
-                        backgroundStyle={props.styles.container}
-                        handleComponent={null}
-                        handleStyle={{ display: "none" }}
-                    >
-                        <BottomSheetScrollView style={[{ flex: 1 }]} contentContainerStyle={{ paddingBottom: 16 }}>
-                            {props.content}
-                        </BottomSheetScrollView>
-                    </BottomSheet>
-                )}
-            </GestureHandlerRootView>
+            {ready && (
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    index={0}
+                    animateOnMount
+                    enableDynamicSizing
+                    maxDynamicContentSize={maxHeight}
+                    containerHeight={windowHeight}
+                    enablePanDownToClose
+                    onChange={handleChange}
+                    onClose={() => handleChange(-1)}
+                    backdropComponent={renderBackdrop}
+                    style={[props.styles.modal]}
+                    backgroundStyle={props.styles.container}
+                    handleComponent={null}
+                    handleStyle={{ display: "none" }}
+                >
+                    <BottomSheetScrollView style={[{ flex: 1 }]} contentContainerStyle={{ paddingBottom: 16 }}>
+                        {props.content}
+                    </BottomSheetScrollView>
+                </BottomSheet>
+            )}
         </Modal>
     );
 };
