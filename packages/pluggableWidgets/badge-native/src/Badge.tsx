fbe6dac19be838cbf68a2f78fbd140ce39cd109e
Yogendra Shelke <yogendrashelke1992@gmail.com>
2026-07-31 13:50:27 +0530

    chore: break widget for testing

--- packages/pluggableWidgets/badge-native/src/Badge.tsx
+++ packages/pluggableWidgets/badge-native/src/Badge.tsx
@@ -1,38 +1,11 @@
-import { flattenStyles } from "@mendix/piw-native-utils-internal";
-import { ReactElement, useCallback } from "react";
-import { Pressable, Text, View } from "react-native";
+import { ReactElement, Fragment } from "react";
 
 import { BadgeProps } from "../typings/BadgeProps";
-import { BadgeStyle, defaultBadgeStyle } from "./ui/Styles";
-import { executeAction, isAvailable } from "@mendix/piw-utils-internal";
+import { BadgeStyle } from "./ui/Styles";
 
 export type Props = BadgeProps<BadgeStyle>;
 
 export function Badge(props: BadgeProps<BadgeStyle>): ReactElement {
-    const styles = flattenStyles(defaultBadgeStyle, props.style);
-
-    const renderText = useCallback(() => {
-        return (
-            <Text testID={`${props.name}$caption`} style={styles.caption}>
-                {isAvailable(props.caption) ? props.caption.value : ""}
-            </Text>
-        );
-    }, [props.caption, styles]);
-
-    return (
-        <View style={styles.container} testID={props.name}>
-            {props.onClick ? (
-                <Pressable
-                    onPress={() => executeAction(props.onClick)}
-                    android_ripple={{
-                        color: styles.container.rippleColor
-                    }}
-                >
-                    {renderText()}
-                </Pressable>
-            ) : (
-                renderText()
-            )}
-        </View>
-    );
+    console.log("Badge props", props);
+    return <Fragment />;
 }
