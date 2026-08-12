8a2ede894ac80ebe56ff358bd0366f4b6090327c
Saurabh Chavan <saurabh.chavan@mendix.com>
2026-08-12 15:37:44 +0530

    fix: migrated class component to functional component

--- packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.tsx
+++ packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.tsx
@@ -1,15 +1,15 @@
 import { flattenStyles } from "@mendix/piw-native-utils-internal";
 import { executeAction } from "@mendix/piw-utils-internal";
 import { Icon } from "mendix/components/native/Icon";
-import { Component, JSX, useEffect, useRef } from "react";
-import { Animated, Easing, Pressable, StyleSheet, Text, View, ViewStyle } from "react-native";
+import { JSX, useEffect, useRef, useState } from "react";
+import { Animated, Easing, Pressable, Text, View, ViewStyle } from "react-native";
 
-import { FloatingActionButtonProps } from "../typings/FloatingActionButtonProps";
-import { defaultFloatingActionButtonStyle, FloatingActionButtonStyle } from "./ui/styles";
-
-interface State {
-    active: boolean;
-}
+import {
+    FloatingActionButtonProps,
+    HorizontalPositionEnum,
+    VerticalPositionEnum
+} from "../typings/FloatingActionButtonProps";
+import { defaultFloatingActionButtonStyle, FloatingActionButtonStyle, styles } from "./ui/styles";
 
 const defaultIconSource = { type: "glyph", iconClass: "glyphicon-plus" } as const;
 const defaultActiveIconSource = { type: "glyph", iconClass: "glyphicon-remove" } as const;
@@ -21,6 +21,66 @@ interface AnimatedMainIconProps {
     style: FloatingActionButtonStyle;
     icon: FloatingActionButtonProps<FloatingActionButtonStyle>["icon"];
     iconActive: FloatingActionButtonProps<FloatingActionButtonStyle>["iconActive"];
+}
+
+interface SecondaryActionItemProps {
+    active: boolean;
+    index: number;
+    direction: "up" | "down";
+    horizontalPosition: "left" | "right" | "center";
+    name: string;
+    button: FloatingActionButtonProps<FloatingActionButtonStyle>["secondaryButtons"][number];
+    style: FloatingActionButtonStyle;
+    mainButtonSize: number;
+    secondaryButtonSize: number;
+    onPress: () => void;
+}
+
+function getVerticalOrientation(verticalPosition: VerticalPositionEnum): "up" | "down" {
+    switch (verticalPosition) {
+        case "bottom":
+            return "up";
+        case "top":
+        default:
+            return "down";
+    }
+}
+
+function getPositionStyle(
+    verticalPosition: VerticalPositionEnum,
+    horizontalPosition: HorizontalPositionEnum
+): ViewStyle {
+    const positionStyle: ViewStyle = {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        zIndex: 999
+    };
+
+    switch (verticalPosition) {
+        case "bottom":
+            positionStyle.bottom = 0;
+            break;
+        case "top":
+        default:
+            positionStyle.top = 0;
+            break;
+    }
+
+    switch (horizontalPosition) {
+        case "left":
+            positionStyle.alignItems = "flex-start";
+            break;
+        case "center":
+            positionStyle.alignItems = "center";
+            break;
+        case "right":
+        default:
+            positionStyle.alignItems = "flex-end";
+            break;
+    }
+
+    return positionStyle;
 }
 
 function AnimatedMainIcon(props: AnimatedMainIconProps): JSX.Element {
@@ -53,19 +113,6 @@ function AnimatedMainIcon(props: AnimatedMainIconProps): JSX.Element {
             </Animated.View>
         </View>
     );
-}
-
-interface SecondaryActionItemProps {
-    active: boolean;
-    index: number;
-    direction: "up" | "down";
-    horizontalPosition: "left" | "right" | "center";
-    name: string;
-    button: FloatingActionButtonProps<FloatingActionButtonStyle>["secondaryButtons"][number];
-    style: FloatingActionButtonStyle;
-    mainButtonSize: number;
-    secondaryButtonSize: number;
-    onPress: () => void;
 }
 
 function SecondaryActionItem(props: SecondaryActionItemProps): JSX.Element {
@@ -193,177 +240,85 @@ function SecondaryActionItem(props: SecondaryActionItemProps): JSX.Element {
     );
 }
 
-export class FloatingActionButton extends Component<FloatingActionButtonProps<FloatingActionButtonStyle>, State> {
-    readonly state: State = {
-        active: false
+export function FloatingActionButton(props: FloatingActionButtonProps<FloatingActionButtonStyle>): JSX.Element {
+    const [active, setActive] = useState(false);
+    const style = flattenStyles(defaultFloatingActionButtonStyle, props.style);
+    const buttonStyle = { ...style.button };
+    delete buttonStyle.rippleColor;
+
+    const mainButtonSize = style.button.size ?? 54;
+    const secondaryButtonSize = style.secondaryButton.size ?? 40;
+    const horizontalPosition = props.horizontalPosition ?? "right";
+    const hasSecondaryButtons = !!props.secondaryButtons?.length;
+
+    const handlePress = (): void => {
+        if (props.secondaryButtons?.length) {
+            setActive(prev => !prev);
+            return;
+        }
+
+        executeAction(props.onClick);
     };
 
-    private readonly onPressHandler = this.onPress.bind(this);
-
-    render(): JSX.Element {
-        const style = flattenStyles(defaultFloatingActionButtonStyle, this.props.style);
-        const buttonStyle = { ...style.button };
-        delete buttonStyle.rippleColor;
-
-        const mainButtonSize = style.button.size ?? 54;
-        const secondaryButtonSize = style.secondaryButton.size ?? 40;
-        const horizontalPosition = this.props.horizontalPosition ?? "right";
-        const hasSecondaryButtons = !!this.props.secondaryButtons?.length;
-
-        return (
-            <View pointerEvents="box-none" style={[styles.wrapper, style.container, this.getPositionStyle()]}>
-                {this.renderButtons(style, mainButtonSize, secondaryButtonSize, horizontalPosition)}
-
-                <Pressable
-                    testID={this.props.name}
-                    onPress={this.onPressHandler}
-                    style={({ pressed }) => [
-                        styles.mainButtonBase,
-                        buttonStyle,
-                        {
-                            width: mainButtonSize,
-                            height: mainButtonSize,
-                            borderRadius: mainButtonSize / 2,
-                            opacity: pressed ? 0.2 : 1
-                        }
-                    ]}
-                >
-                    <AnimatedMainIcon
-                        active={this.state.active}
-                        hasSecondaryButtons={hasSecondaryButtons}
-                        style={style}
-                        icon={this.props.icon}
-                        iconActive={this.props.iconActive}
-                    />
-                </Pressable>
-            </View>
-        );
-    }
-
-    private renderButtons(
+    const renderButtons = (
         style: FloatingActionButtonStyle,
         mainButtonSize: number,
         secondaryButtonSize: number,
         horizontalPosition: "left" | "right" | "center"
-    ): JSX.Element[] | undefined {
-        return this.props.secondaryButtons?.map((button, index) => (
+    ): JSX.Element[] | undefined => {
+        return props.secondaryButtons?.map((button, index) => (
             <SecondaryActionItem
                 key={`button${index}`}
-                active={this.state.active}
+                active={active}
                 index={index}
-                direction={this.verticalOrientation}
+                direction={getVerticalOrientation(props.verticalPosition)}
                 horizontalPosition={horizontalPosition}
-                name={this.props.name}
+                name={props.name}
                 button={button}
                 style={style}
                 mainButtonSize={mainButtonSize}
                 secondaryButtonSize={secondaryButtonSize}
                 onPress={() => {
-                    this.setState({ active: false });
+                    setActive(false);
                     executeAction(button.onClick);
                 }}
             />
         ));
-    }
+    };
 
-    private get verticalOrientation(): "up" | "down" {
-        switch (this.props.verticalPosition) {
-            case "bottom":
-                return "up";
-            case "top":
-                return "down";
-            default:
-                return "down";
-        }
-    }
+    return (
+        <View
+            pointerEvents="box-none"
+            style={[
+                styles.wrapper,
+                style.container,
+                getPositionStyle(props.verticalPosition, props.horizontalPosition)
+            ]}
+        >
+            {renderButtons(style, mainButtonSize, secondaryButtonSize, horizontalPosition)}
 
-    private getPositionStyle(): ViewStyle {
-        const positionStyle: ViewStyle = {
-            position: "absolute",
-            left: 0,
-            right: 0,
-            zIndex: 999
-        };
-
-        switch (this.props.verticalPosition) {
-            case "bottom":
-                positionStyle.bottom = 0;
-                break;
-            case "top":
-            default:
-                positionStyle.top = 0;
-                break;
-        }
-
-        switch (this.props.horizontalPosition) {
-            case "left":
-                positionStyle.alignItems = "flex-start";
-                break;
-            case "center":
-                positionStyle.alignItems = "center";
-                break;
-            case "right":
-            default:
-                positionStyle.alignItems = "flex-end";
-                break;
-        }
-
-        return positionStyle;
-    }
-
-    private onPress(): void {
-        if (this.props.secondaryButtons?.length) {
-            // eslint-disable-next-line react/no-access-state-in-setstate
-            this.setState({ active: !this.state.active });
-            return;
-        }
-
-        executeAction(this.props.onClick);
-    }
+            <Pressable
+                testID={props.name}
+                onPress={handlePress}
+                style={({ pressed }) => [
+                    styles.mainButtonBase,
+                    buttonStyle,
+                    {
+                        width: mainButtonSize,
+                        height: mainButtonSize,
+                        borderRadius: mainButtonSize / 2,
+                        opacity: pressed ? 0.2 : 1
+                    }
+                ]}
+            >
+                <AnimatedMainIcon
+                    active={active}
+                    hasSecondaryButtons={hasSecondaryButtons}
+                    style={style}
+                    icon={props.icon}
+                    iconActive={props.iconActive}
+                />
+            </Pressable>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-    wrapper: {
-        justifyContent: "flex-end"
-    },
-    mainButtonBase: {
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    secondaryAnchor: {
-        position: "absolute"
-    },
-    secondaryRow: {
-        width: "100%",
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    rowAlignLeft: {
-        justifyContent: "flex-start"
-    },
-    rowAlignRight: {
-        justifyContent: "flex-end"
-    },
-    rowAlignCenter: {
-        justifyContent: "center"
-    },
-    secondaryButtonBase: {
-        alignItems: "center",
-        justifyContent: "center",
-        flexShrink: 0
-    },
-    captionInlineContainer: {
-        flexShrink: 0
-    },
-    captionBeforeButton: {
-        marginRight: 8,
-        alignItems: "flex-end"
-    },
-    captionAfterButton: {
-        marginLeft: 8,
-        alignItems: "flex-start"
-    },
-    captionText: {
-        flexShrink: 0
-    }
-});
